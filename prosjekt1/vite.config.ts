ebe764a26a13e3aae17049a48ad8c54329a3710a
Leif Eggenfellner <eggenfellner@protonmail.com>
2024-09-10 12:40:14 +0200

    feat: add path alias

--- prosjekt1/vite.config.ts
+++ prosjekt1/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -7,10 +8,22 @@ export default defineConfig({
     server: {
         open: true,
     },
+    resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+          '@assets': path.resolve(__dirname, './src/assets'),
+          '@components': path.resolve(__dirname, './src/components'),
+          '@hooks': path.resolve(__dirname, './src/hooks'),
+          '@interfaces': path.resolve(__dirname, './src/interfaces'),
+          '@routes': path.resolve(__dirname, './src/routes'),
+          '@styles': path.resolve(__dirname, './src/styles'),
+          '@utils': path.resolve(__dirname, './src/utils'),
+        }
+      },
     test: {
         globals: true,
         environment: "jsdom",
-        include: ['./src/tests/**/*.test.tsx', './src//tests/**/*.test.ts'],
+        include: ['./src/tests/**/*.test.tsx', './src/tests/**/*.test.ts'],
         setupFiles: ['./src/tests/setup.ts'],
     }
 });
