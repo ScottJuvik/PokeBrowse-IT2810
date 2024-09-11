bac8862540c37eea2f1dbd694f835160d89df9f4
Leif Eggenfellner <eggenfellner@protonmail.com>
2024-09-11 16:13:23 +0200

    test: updated to test new home page

--- prosjekt1/src/tests/routes/home/home.test.tsx
+++ prosjekt1/src/tests/routes/home/home.test.tsx
@@ -1,14 +1,13 @@
 import Home from '@routes/home/home.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, screen } from '@testing-library/react';
-import React from 'react';
+import { render, screen, within } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 
 const queryClient = new QueryClient();
 
 describe('Home component', () => {
-    it("renders 'Hello World' on the home route", () => {
+    it("renders 'PokéBrowse' title on the home route", () => {
         const router = createMemoryRouter(
             [
                 {
@@ -27,6 +26,11 @@ describe('Home component', () => {
             </QueryClientProvider>
         );
 
-        expect(screen.getByText('Hello World')).toBeInTheDocument();
+        expect(within(screen.getByRole('heading', { level: 1 })).getByText(/Poké/i)).toBeInTheDocument();
+
+        expect(within(screen.getByRole('heading', { level: 1 })).getByText(/Browse/i)).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Search for a Pokémon')).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /Random/i })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /Pokémon/i })).toBeInTheDocument();
     });
 });
