36be1fdd103de0222e9d2ab85ce257d0b5672391
Leif Eggenfellner <eggenfellner@protonmail.com>
2024-09-12 11:34:57 +0200

    feat: add storybook
    Added storybook to the project to allow developers to create story for components to view them while developing.

---
+++ prosjekt1/src/stories/components/common/button/button.stories.tsx
@@ -0,0 +1,80 @@
+import Button, { ButtonProps, ButtonSize, ButtonType } from '@components/common/button/button.tsx';
+import { Meta, StoryFn } from '@storybook/react';
+import styles from './button.module.css';
+
+export default {
+    title: 'Components/Button',
+    component: Button,
+    argTypes: {
+        onClick: { action: 'clicked' },
+        type: {
+            control: {
+                type: 'select',
+                options: ButtonType,
+            },
+        },
+        size: {
+            control: {
+                type: 'select',
+                options: ButtonSize,
+            },
+        },
+        disabled: {
+            control: 'boolean',
+        },
+        customClassName: {
+            control: 'text',
+        },
+        style: {
+            control: 'object',
+        },
+    },
+} as Meta;
+
+const Template: StoryFn<ButtonProps> = args => <Button {...args} />;
+
+// Default Button story (Filled & Medium)
+export const Default = Template.bind({});
+Default.args = {
+    text: 'Default Button',
+    type: ButtonType.Filled,
+    size: ButtonSize.Medium,
+    disabled: false,
+};
+
+// Filled Button
+export const Filled = Template.bind({});
+Filled.args = {
+    text: 'Filled Button',
+    type: ButtonType.Filled,
+    size: ButtonSize.Large,
+    disabled: false,
+};
+
+// Outline Button
+export const Outline = Template.bind({});
+Outline.args = {
+    text: 'Outline Button',
+    type: ButtonType.Outline,
+    size: ButtonSize.Small,
+    disabled: false,
+};
+
+// Disabled Button
+export const Disabled = Template.bind({});
+Disabled.args = {
+    text: 'Disabled Button',
+    type: ButtonType.Filled,
+    size: ButtonSize.Medium,
+    disabled: true,
+};
+
+// Custom Class and Style Button
+export const CustomStyle = Template.bind({});
+CustomStyle.args = {
+    text: 'Custom Button',
+    type: ButtonType.Outline,
+    size: ButtonSize.Medium,
+    customClassName: styles['custom-class'],
+    style: { backgroundColor: 'purple' },
+};
