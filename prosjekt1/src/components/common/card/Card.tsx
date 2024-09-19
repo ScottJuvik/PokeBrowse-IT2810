54ccd8a568cd2f8af2b39fc9ac9a9f980349e403
Sepanta Ganjei <sepantag@stud.ntnu.no>
2024-09-19 18:17:27 +0200

    feat: update card

--- prosjekt1/src/components/common/card/Card.tsx
+++ prosjekt1/src/components/common/card/Card.tsx
@@ -2,18 +2,19 @@ import { Badge } from '@/components/ui/badge/Badge';
 import { usePokemon } from '@/hooks/usePokemon';
 import { MinimalPokemon } from '@/interfaces/pokemon';
 import { formatPokemonName } from '@/utils/text';
-import { useNavigate } from 'react-router-dom';
 import whosThatPokemon from '@assets/who.jpg';
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FavoriteButton from './FavoriteButton';
 import SkeletonLoader from './SkeletonLoader';
 import styles from './styles/Card.module.css';
 
-export interface CardProps {
+interface CardProps {
     nameOrId: string | number;
+    onToggleFavorite?: () => void;
 }
 
-const Card = ({ nameOrId }: CardProps) => {
+const Card = ({ nameOrId, onToggleFavorite }: CardProps) => {
     const [isFavourite, setIsFavourite] = useState(false);
     const {
         data: pokemon,
@@ -27,12 +28,45 @@ const Card = ({ nameOrId }: CardProps) => {
 
     const navigate = useNavigate();
 
-    const handleCardClick = () => {
-        navigate(`/project1/pokemon/${nameOrId}`);
+    useEffect(() => {
+        if (pokemon) {
+            const storedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+            if (storedFavorites.includes(pokemon.id)) {
+                setIsFavourite(true);
+            }
+        }
+    }, [pokemon]);
+
+    if (isLoading) {
+        return <SkeletonLoader />;
     }
+
+    if (error || !pokemon) {
+        console.error('Error loading Pokémon data:', error);
+        return <p>Error loading Pokémon data</p>;
+    }
+
+    const handleCardClick = () => {
+        navigate(`/project1/pokemon/${pokemon.name}`);
+    };
 
     const toggleFavourite = () => {
         setIsFavourite(!isFavourite);
+
+        const storedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+        let updatedFavorites;
+        if (storedFavorites.includes(pokemon.id)) {
+            updatedFavorites = storedFavorites.filter((favIndex: number) => favIndex !== pokemon.id);
+        } else {
+            updatedFavorites = [...storedFavorites, pokemon.id];
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+
+        if (onToggleFavorite) {
+            onToggleFavorite();
+        }
     };
 
     if (isLoading) {
