aae638974ffacdc9395b7604ba54dafe02764cc9
Scott Juvik <scottj@stud.ntnu.no>
2024-09-20 10:37:21 +0200

    feat: :sparkles: add drill-down navigation page from pokemon list to detail view

---
+++ prosjekt1/src/components/common/LargePokemonCard/PokemonStats/PokemonStats.tsx
@@ -0,0 +1,44 @@
+// components/ui/stats/PokemonStats.tsx
+import styles from './PokemonStats.module.css';
+
+interface PokemonStatsProps {
+    hp: number;
+    attack: number;
+    defense: number;
+    specialAttack: number;
+    specialDefense: number;
+    speed: number;
+}
+
+const PokemonStats = ({ hp, attack, defense, specialAttack, specialDefense, speed }: PokemonStatsProps) => {
+    return (
+        <div className={styles.pokemonStats}>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>HP</span>
+                <span className={styles.statValue}>{hp}</span>
+            </div>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Attack</span>
+                <span className={styles.statValue}>{attack}</span>
+            </div>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Defense</span>
+                <span className={styles.statValue}>{defense}</span>
+            </div>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Special Attack</span>
+                <span className={styles.statValue}>{specialAttack}</span>
+            </div>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Special Defense</span>
+                <span className={styles.statValue}>{specialDefense}</span>
+            </div>
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Speed</span>
+                <span className={styles.statValue}>{speed}</span>
+            </div>
+        </div>
+    );
+};
+
+export default PokemonStats;
---
+++ prosjekt1/src/routes/pokemonView/PokemonViewPage.tsx
@@ -0,0 +1,76 @@
+import LargePokemonCard from '@/components/common/LargePokemonCard/LargePokemonCard';
+import Loader from '@/components/common/loader/Loader';
+import { usePokemon } from '@/hooks/usePokemon';
+import ErrorPage from '@/routes/errors/ErrorPage';
+import { useNavigate, useParams } from 'react-router-dom';
+import styles from './pokemonView.module.css';
+
+const PokemonViewPage = () => {
+    const { idOrName } = useParams<{ idOrName: string }>();
+    const navigate = useNavigate();
+
+    if (!idOrName) {
+        return <div>Error: Pokémon ID or Name is undefined</div>;
+    }
+
+    const { data: pokemon, isLoading, isError } = usePokemon(idOrName);
+
+    if (isLoading) {
+        return (
+            <div className={styles.pokemonViewContainer}>
+                <Loader />
+            </div>
+        );
+    }
+
+    if (isError || !pokemon) {
+        return <ErrorPage />;
+    }
+
+    const { name, sprites, stats, abilities, types } = pokemon;
+
+    const hp = stats.find((stat: any) => stat.stat.name === 'hp').base_stat;
+    const attack = stats.find((stat: any) => stat.stat.name === 'attack').base_stat;
+    const defense = stats.find((stat: any) => stat.stat.name === 'defense').base_stat;
+    const specialAttack = stats.find((stat: any) => stat.stat.name === 'special-attack').base_stat;
+    const specialDefense = stats.find((stat: any) => stat.stat.name === 'special-defense').base_stat;
+    const speed = stats.find((stat: any) => stat.stat.name === 'speed').base_stat;
+
+    const parsedAbilities = abilities.map((ability: any) => ability.ability.name).join(', ');
+    const parsedTypes = types.map((type: any) => type.type.name);
+
+    const nextPokemon = () => {
+        const nextId = Number(pokemon.id) + 1;
+        navigate(`/pokemon/${nextId}`);
+    };
+
+    const prevPokemon = () => {
+        const prevId = Number(pokemon.id) - 1;
+        if (prevId > 0) {
+            navigate(`/pokemon/${prevId}`);
+        }
+    };
+
+    return (
+        <div className={styles.pokemonViewContainer}>
+            <LargePokemonCard
+                name={name.charAt(0).toUpperCase() + name.slice(1)}
+                index={pokemon.id}
+                imageUrl={sprites.other['official-artwork'].front_default}
+                hp={hp}
+                attack={attack}
+                defense={defense}
+                specialAttack={specialAttack}
+                specialDefense={specialDefense}
+                speed={speed}
+                abilities={parsedAbilities}
+                types={parsedTypes}
+                description="The more sunlight Ivysaur bathes in, the more strength wells up within it, allowing the bud on its back to grow larger."
+                nextPokemon={nextPokemon}
+                prevPokemon={prevPokemon}
+            />
+        </div>
+    );
+};
+
+export default PokemonViewPage;
